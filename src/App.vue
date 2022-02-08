8405de20c8ae73b99a7b4227ccd0ac6c078a4325
OyakiMorse <legenda1200@gmail.com>
2022-02-08 14:56:23 +0200

    Usage named routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/login">About</router-link>
+    <router-link :to="{ name: 'Home' }">Home</router-link> |
+    <router-link :to="{ name: 'Login' }">About</router-link>
   </div>
   <router-view />
 </template>
